3459da288f75b5db9f2dcfb46e680ab8613a2182
bhavik shah <bhavik shah>
2017-02-02 11:31:30 +0530

    initial commit

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['NODE_NEW_RELIC_DEMO'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '309763036b8a5761226dd64b9d08ad35d859f836',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
